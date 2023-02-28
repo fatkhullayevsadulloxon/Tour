adb23c3898deff241f75167172cdbb06b9b7defa
fatkhullayevsadulloxon <sadulloxonfatkhullayev@gmail.com>
2023-02-28 22:37:06 +0000

    init

--- src/components/korzina/Korzina.js
+++ src/components/korzina/Korzina.js
@@ -16,13 +16,7 @@ export const Korzina = () => {
                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
-                            <Link className="d-flex" to={`travel/`}>
-                                <img src={korzina.img} alt="" width={50} />
-                                <div>
-                                    <h3>{korzina.name}</h3>
-                                    <p>{korzina.description}</p>
-                                </div>
-                            </Link>
+
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
